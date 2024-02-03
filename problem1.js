b7e494e00ab127175656bd365f58ab772fbd270f
Shantanu <shantanujha512@gmail.com>
2024-02-03 12:24:56 +0530

    problem_1

---
+++ problem1.js
@@ -0,0 +1,8 @@
+console.log("Student Name:Roshan Kumar");
+console.log("School Name:Masai School");
+console.log("Grade:6");
+console.log("Roll Number:5");
+console.log("*********Marks out of 100********");
+console.log("  science= 90");
+console.log("  maths  = 65");
+console.log("  physics= 76");
